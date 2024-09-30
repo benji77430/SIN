c23e56d061c471bb34aeed1e41db9b18e3488de9
benji77430 <85637652+benji77430@users.noreply.github.com>
2024-09-30 14:46:32 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,5 @@
+curl -o sin.py https://raw.githubusercontent.com/benji77430/SIN/refs/heads/main/sin.py
+curl -o sin.py https://raw.githubusercontent.com/benji77430/SIN/refs/heads/main/requirements.txt
+
+pip install -r requirements.txt --break-system-packages
+python sin.py
